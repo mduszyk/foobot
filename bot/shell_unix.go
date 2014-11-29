18e63c61d0cd1548b63e7beab3e5f0e86ead829f
mike <michal@duszyk.com>
2014-11-29 16:59:24 +0100

    added read timeout to unix shell

--- bot/shell_unix.go
+++ bot/shell_unix.go
@@ -1,21 +1,60 @@
 package bot
 
+/*
+#include <stdlib.h>
+#include <sys/select.h>
+
+static void _FD_ZERO(void *set) {
+    FD_ZERO((fd_set*)set);
+}
+
+static void _FD_SET(int sysfd, void *set) {
+    FD_SET(sysfd, (fd_set*)set);
+}
+
+static int _FD_ISSET (int sysfd, void *set) {
+    return FD_ISSET(sysfd, (fd_set*)set);
+}
+*/
+import "C"
+
 import(
-    "io"
     "os"
     "os/exec"
+    "syscall"
+    "unsafe"
     "strings"
     "github.com/kr/pty"
     "github.com/mduszyk/foobot/log"
     "github.com/mduszyk/foobot/conf"
 )
 
+func FD_ZERO(set *syscall.FdSet) {
+    s := unsafe.Pointer(set)
+    C._FD_ZERO(s)
+}
+
+func FD_SET(sysfd int, set *syscall.FdSet) {
+    s := unsafe.Pointer(set)
+    fd := C.int(sysfd)
+    C._FD_SET(fd, s)
+}
+
+func FD_ISSET(sysfd int, set *syscall.FdSet) bool {
+    s := unsafe.Pointer(set)
+    fd := C.int(sysfd)
+    return C._FD_ISSET(fd, s) != 0
+}
+
 const PS1 = "--FoObOt--"
+
 
 type Shell struct {
     cmd *exec.Cmd
     pty *os.File
     ps1 string
+    fds *syscall.FdSet
+    timeout *syscall.Timeval
 }
 
 func NewShell() *Shell {
@@ -64,31 +103,64 @@ func (sh *Shell) setupPrompt(ps1 string) {
     log.TRACE.Printf("Prompt ready: %s", ps1)
 }
 
-func readBetween(r io.Reader, token1 string, token2 string) string {
+func readBetween(f *os.File, token1 string, token2 string) string {
     buf := make([]byte, 256)
     var outBuf []byte
-    start := -1
+    start := 0
     end := 0
+    first := true
+
+    fd := int(f.Fd())
+    /* log.TRACE.Printf("Read, fd: %d", fd) */
+
+    rfds := &syscall.FdSet{}
+
+    timeout := &syscall.Timeval{}
+    timeout.Sec = int64(0)
+    timeout.Usec = int64(500000)
 
     for {
-        n, _ := r.Read(buf)
+        FD_ZERO(rfds)
+        FD_SET(fd, rfds)
+
+        n, err := syscall.Select(fd + 1, rfds, nil, nil, timeout)
+        if err != nil {
+            if err == syscall.EINTR {
+                log.TRACE.Printf("Select interrupted")
+                continue
+            } else {
+                log.ERROR.Printf("Select failed, error: %s", err)
+                break
+            }
+        }
+        if !FD_ISSET(fd, rfds) {
+            log.TRACE.Printf("Select timeout")
+            break
+        }
+
+        n, err = f.Read(buf)
+        if err != nil {
+            log.ERROR.Printf("Failed reading shell output, error: %s", err)
+            break
+        }
+
         end += n
         outBuf = append(outBuf, buf[:n]...)
         text := string(outBuf[:end])
         /* log.TRACE.Printf("text: %s, token2: %s", text, token2) */
 
-        if start < 0 {
+        if first {
             start = strings.Index(text, token1) + len(token1)
+            first = false
             /* log.TRACE.Printf("start: %d", start) */
         }
         if strings.Contains(text, token2) {
+            end -= len(token2)
             break
         }
     }
 
     /* log.TRACE.Printf("out: %s", string(outBuf[:end])) */
-
-    end -= len(token2)
     return string(outBuf[start:end])
 }
 
@@ -101,4 +173,3 @@ func (sh *Shell) Insert(line string) string {
 
     return readBetween(sh.pty, "\n", sh.ps1)
 }
-
