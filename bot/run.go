7c2b278397991b5b677a3566054dc7a43716844e
maro <maro>
2015-01-01 17:39:24 +0100

    Run module done.

--- bot/run.go
+++ bot/run.go
@@ -1,7 +1,10 @@
 package bot
 
 import(
+    "os/exec"
+    "strings"
     "github.com/mduszyk/foobot/proto"
+    "github.com/mduszyk/foobot/log"
 )
 
 type Run struct {
@@ -11,9 +14,29 @@ func NewRunModule() *Run {
     return &Run{}
 }
 
+func runCommand(cmd string, args string) string {
+    log.TRACE.Printf("Command to execute: %s", cmd)
+
+    arg_list := strings.Split(args, " ");
+    log.TRACE.Printf("Argument list: %s", arg_list)
+    log.TRACE.Printf("Argument's lenght: %d", len(arg_list))
+
+    out, err := exec.Command(cmd, arg_list...).Output()
+    if err != nil {
+        log.ERROR.Printf("Failed executing command %s, arg_list = %s, error: %s", cmd, arg_list, err)
+    }
+    log.TRACE.Printf("Command %s successfully executed. Output: %s", cmd, string(out[:]))
+    return string(out[:])
+}
+
 func (r *Run) Handle(msg *proto.Msg) string {
     rsp := ""
-    rsp = "run module invoked. WIP."
+
+    if len(msg.Cmd) == 0 {
+        rsp = "Command is empty"
+    } else {
+        rsp = runCommand(msg.Cmd, msg.Args)
+    }
 
     return rsp
 }
