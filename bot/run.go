bdf04c149336c9f45fbcd8a6ce087f2137bf2c6d
maro <maro>
2015-01-01 17:45:51 +0100

    Run module fixes.

--- bot/run.go
+++ bot/run.go
@@ -24,8 +24,10 @@ func runCommand(cmd string, args string) string {
     out, err := exec.Command(cmd, arg_list...).Output()
     if err != nil {
         log.ERROR.Printf("Failed executing command %s, arg_list = %s, error: %s", cmd, arg_list, err)
+    } else {
+        log.INFO.Printf("Command %s successfully executed.", cmd)
+        log.TRACE.Printf("Command's output: %s", out)
     }
-    log.TRACE.Printf("Command %s successfully executed. Output: %s", cmd, string(out[:]))
     return string(out[:])
 }
 
