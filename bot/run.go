ee036db7558bf3c36f33ea24072057ef108f53dc
maro <maro>
2015-01-02 16:58:13 +0100

    Run module fixes + trace logger added.

--- bot/run.go
+++ bot/run.go
@@ -20,20 +20,25 @@ func runCommand(cmd string, args string) string {
     arg_list := strings.Fields(args);
     log.TRACE.Printf("Argument's lenght: %d", len(arg_list))
     if len(arg_list) != 0 {
-        log.TRACE.Printf("Argument list%", arg_list)
+        log.TRACE.Printf("Argument list %s", arg_list)
     }
 
     var out []byte
     var err error
     if len(arg_list) == 0 {
+        log.TRACE.Printf("Executing command %s with no arguments", cmd)
         out, err = exec.Command(cmd).CombinedOutput()
     } else {
+        log.TRACE.Printf("Executing command %s with arguments %s",
+                         cmd, arg_list)
         out, err = exec.Command(cmd, arg_list...).CombinedOutput()
     }
 
     if err != nil {
         log.ERROR.Printf(`Failed executing command %s,
-                                arg_list = %s, error: %s`, cmd, arg_list, err)
+                         arg_list = %s, error: %s`, cmd, arg_list, err)
+    } else if out == nil{
+        log.TRACE.Printf("Output is nil")
     } else {
         log.INFO.Printf("Command %s successfully executed.", cmd)
         log.TRACE.Printf("Command's output: %s", out)
